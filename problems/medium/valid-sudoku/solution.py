374a9a0ae8afa58713f8f4fda9491492cd27d3a9
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-11-03 20:02:08 -0500

    Modify soln

--- problems/medium/valid-sudoku/solution.py
+++ problems/medium/valid-sudoku/solution.py
@@ -10,26 +10,30 @@ from typing import List
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         for row in board:
-            if self.containsDuplicate(row):
-                return False
+            s = set()
+            for val in row:
+                if self.containsDuplicate(val, s):
+                    return False
 
-        for col in range(0, 9):
-            lst = []
-            for row in range(0, 9):
-                lst.append(board[row][col])
-            if self.containsDuplicate(lst):
-                return False
+        for col in range(9):
+            s = set()
+            for row in range(9):
+                if self.containsDuplicate(board[row][col], s):
+                    return False
 
         for row in range(0, 9, 3):
             for col in range(0, 9, 3):
-                lst = []
+                s = set()
                 for sub_row in range(row, row + 3):
-                    lst.extend(board[sub_row][col : col + 3])
-                if self.containsDuplicate(lst):
-                    return False
+                    for sub_col in range(col, col + 3):
+                        if self.containsDuplicate(board[sub_row][sub_col], s):
+                            return False
 
         return True
 
-    def containsDuplicate(self, lst: List[str]) -> bool:
-        only_nums = [x for x in lst if x != "."]
-        return len(only_nums) != len(set(only_nums))
+    def containsDuplicate(self, val: str, s: set) -> bool:
+        if val in s:
+            return True
+        if val != ".":
+            s.add(val)
+        return False
