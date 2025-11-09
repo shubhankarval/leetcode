d83074840dd80c9cedd28eaf7b173cd536746657
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-11-09 10:23:46 -0500

    FInal soln for 3sum

--- problems/medium/3sum/solution.py
+++ problems/medium/3sum/solution.py
@@ -9,31 +9,26 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res, seen = [], set()
-        num_idx = self.create_index_map(nums)
+        nums.sort()
+        res = []
 
         for i in range(len(nums) - 2):
-            for j in range(i + 1, len(nums) - 1):
-                target = 0 - (nums[i] + nums[j])
-                fs = frozenset([nums[i], nums[j], target])
-                if fs in seen:
-                    continue
-                seen.add(fs)
-                if target in num_idx and self.check_dup_idx(num_idx[target], i, j):
-                    res.append([nums[i], nums[j], target])
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
         return res
-
-    def create_index_map(self, lst: List[int]):
-        index_map = {}
-        for i, val in enumerate(lst):
-            if val not in index_map:
-                index_map[val] = set()
-            index_map[val].add(i)
-        return index_map
-
-    def check_dup_idx(self, s: set, num1: int, num2: int) -> bool:
-        if (num1 in s and num2 not in s) or (num1 not in s and num2 in s):
-            return len(s) > 1
-        elif num1 in s and num2 in s:
-            return len(s) > 2
-        return True
