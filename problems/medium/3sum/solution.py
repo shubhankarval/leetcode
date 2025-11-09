1378de5e7d7eca3d058e44cb356d88fd6cc6318f
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-11-09 07:00:19 -0500

    Updated 3sum soln

--- problems/medium/3sum/solution.py
+++ problems/medium/3sum/solution.py
@@ -9,14 +9,31 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        sets = []
+        res, seen = [], set()
+        num_idx = self.create_index_map(nums)
+
         for i in range(len(nums) - 2):
             for j in range(i + 1, len(nums) - 1):
-                for k in range(j + 1, len(nums)):
-                    lst = [nums[i], nums[j], nums[k]]
-                    s = set(lst)
-                    if nums[i] + nums[j] + nums[k] == 0 and s not in sets:
-                        res.append(lst)
-                        sets.append(s)
+                target = 0 - (nums[i] + nums[j])
+                fs = frozenset([nums[i], nums[j], target])
+                if fs in seen:
+                    continue
+                seen.add(fs)
+                if target in num_idx and self.check_dup_idx(num_idx[target], i, j):
+                    res.append([nums[i], nums[j], target])
         return res
+
+    def create_index_map(self, lst: List[int]):
+        index_map = {}
+        for i, val in enumerate(lst):
+            if val not in index_map:
+                index_map[val] = set()
+            index_map[val].add(i)
+        return index_map
+
+    def check_dup_idx(self, s: set, num1: int, num2: int) -> bool:
+        if (num1 in s and num2 not in s) or (num1 not in s and num2 in s):
+            return len(s) > 1
+        elif num1 in s and num2 in s:
+            return len(s) > 2
+        return True
