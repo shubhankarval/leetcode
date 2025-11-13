2f2683f2588a28d8b4529e1eaaacdbe0d73141bf
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-11-13 14:36:53 -0500

    Solve MinStack

---
+++ problems/medium/min-stack/solution.py
@@ -0,0 +1,32 @@
+"""
+Problem: Min Stack
+Difficulty: Medium
+URL: https://leetcode.com/problems/min-stack/
+
+Time Complexity: O(1) for each operation
+Space Complexity: O(n)
+"""
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minStack:
+            self.minStack.append(val)
+        else:
+            self.minStack.append(min(val, self.minStack[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
