5c88b81cbf121d932ea80ccb35728f3183a2bec9
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-11-13 15:24:10 -0500

    Solve Evaluate RPN

---
+++ problems/medium/eval-rpn/solution.py
@@ -0,0 +1,25 @@
+"""
+Problem: Evaluate Reverse Polish Notation
+Difficulty: Medium
+URL: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+
+from typing import List
+import math
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        res = []
+        for t in tokens:
+            if t in ["+", "-", "*", "/"]:
+                val = eval(res[-2] + t + res[-1])
+                res[-2] = str(math.floor(val) if val > 0 else math.ceil(val))
+                res.pop()
+            else:
+                res.append(t)
+
+        return int(res[-1])
