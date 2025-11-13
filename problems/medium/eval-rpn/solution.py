89623f043ca9240c03ff0e278aae8c3800c0b0cf
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-11-13 15:33:19 -0500

    Update soln for Eval RPN

--- problems/medium/eval-rpn/solution.py
+++ problems/medium/eval-rpn/solution.py
@@ -15,9 +15,9 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         res = []
         for t in tokens:
-            if t in ["+", "-", "*", "/"]:
-                val = eval(res[-2] + t + res[-1])
-                res[-2] = str(math.floor(val) if val > 0 else math.ceil(val))
+            if t in "+-*/":
+                val = int(eval(res[-2] + t + res[-1]))
+                res[-2] = str(val)
                 res.pop()
             else:
                 res.append(t)
