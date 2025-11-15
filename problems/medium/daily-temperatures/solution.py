d9a77df00897f1d0f6e0f346da66fefaa36d2944
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-11-15 06:54:26 -0500

    Solve daily temperatures optimally

--- problems/medium/daily-temperatures/solution.py
+++ problems/medium/daily-temperatures/solution.py
@@ -3,23 +3,20 @@ Problem: Daily Temperatures
 Difficulty: Medium
 URL: https://leetcode.com/problems/daily-temperatures/
 
-Time Complexity: O(n²)
-Space Complexity: O(1)
+Time Complexity: O(n)
+Space Complexity: O(n)
 """
+
+from typing import List
 
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        i, j = 0, 1
-        res = []
-        while i < len(temperatures):
-            while j < len(temperatures) and temperatures[i] >= temperatures[j]:
-                j += 1
-            if j == len(temperatures):
-                res.append(0)
-            else:
-                res.append(j - i)
-            i += 1
-            j = i + 1
-
+        res = [0] * len(temperatures)
+        stack = []
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                j = stack.pop()
+                res[j] = i - j
+            stack.append(i)
         return res
