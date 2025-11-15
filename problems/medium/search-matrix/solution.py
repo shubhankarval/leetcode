5a5242e8cc11553393e4fd4841af310e9764c8a8
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-11-15 16:45:26 -0500

    one pass soln for search a 2D matrix

--- problems/medium/search-matrix/solution.py
+++ problems/medium/search-matrix/solution.py
@@ -12,26 +12,15 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row = -1
-        t, b = 0, len(matrix) - 1
-        while t <= b:
-            m = t + (b - t) // 2
-            if matrix[m][0] <= target <= matrix[m][-1]:
-                row = m
-                break
-            elif target > matrix[m][-1]:
-                t = m + 1
+        rows, cols = len(matrix), len(matrix[0])
+        l, r = 0, (rows * cols) - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            row, col = m // cols, m % cols
+            if target > matrix[row][col]:
+                l = m + 1
+            elif target < matrix[row][col]:
+                r = m - 1
             else:
-                b = m - 1
-        if row != -1:
-            l, r = 0, len(matrix[0]) - 1
-            while l <= r:
-                m = l + (r - l) // 2
-                if target > matrix[row][m]:
-                    l = m + 1
-                elif target < matrix[row][m]:
-                    r = m - 1
-                else:
-                    return True
-
+                return True
         return False
