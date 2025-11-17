524bcbde608b31db7ae8f3f04e64cf6e48b93e30
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-11-17 06:33:11 -0500

    Final soln for Koko Eating Bananas

--- problems/medium/koko-eating-bananas/solution.py
+++ problems/medium/koko-eating-bananas/solution.py
@@ -16,20 +16,15 @@ class Solution:
         s1, s2 = 1, max(piles)
         speed = 0
         while s1 <= s2:
-            currSpeed = s1 + (s2 - s1) // 2
-            currHours = self.getHours(piles, currSpeed)
-            if currHours > hours:
-                s1 = currSpeed + 1  # increase speed to decrease time
-            elif currHours < hours:
+            currSpeed = (s1 + s2) // 2
+            currHours = 0
+            for pile in piles:
+                currHours += math.ceil(pile / currSpeed)
+
+            if currHours <= hours:
                 s2 = currSpeed - 1  # decrease speed to increase time
                 speed = currSpeed
             else:
-                s2 = currSpeed - 1  # continue search to find min speed
-                speed = currSpeed
-        return speed
+                s1 = currSpeed + 1  # increase speed to decrease time
 
-    def getHours(self, piles: List[int], k: int) -> int:
-        hours = 0
-        for pile in piles:
-            hours += math.ceil(pile / k)
-        return hours
+        return speed
