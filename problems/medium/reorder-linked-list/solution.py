7878a79827af2b88e429fc6e12caaeea00532e35
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-12-06 10:42:06 -0600

    Reorder Linked List - optimal

--- problems/medium/reorder-linked-list/solution.py
+++ problems/medium/reorder-linked-list/solution.py
@@ -4,7 +4,7 @@ Difficulty: Medium
 URL: https://leetcode.com/problems/reorder-list/
 
 Time Complexity: O(n)
-Space Complexity: O(n)
+Space Complexity: O(1)
 """
 
 from typing import Optional
@@ -19,15 +19,16 @@ from typing import Optional
 
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        lst = []
-        node = head
-        while node:
-            lst.append(node)
-            node = node.next
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        n1, n2 = head, self.reverseList(slow)
+        while n1 and n2 and n1.next != n2:
+            n1.next, n2.next, n1, n2 = n2, n1.next, n1.next, n2.next
 
-        l, r = 0, len(lst) - 1
-        while l < r:
-            lst[l].next, lst[r].next = lst[r], lst[l].next
-            l += 1
-            r -= 1
-        lst[l].next = None
+    def reverseList(self, head: Optional[ListNode]):
+        prev, curr = None, head
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        return prev
