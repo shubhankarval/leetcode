76d6a327f61efc4afa343fcfa118ccc36e8fb6f4
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-12-11 06:15:36 -0500

    Merge K Sorted Linked Lists - optimal

--- problems/hard/merge-k-lists/solution.py
+++ problems/hard/merge-k-lists/solution.py
@@ -3,8 +3,8 @@ Problem: Merge K Sorted Linked Lists
 Difficulty: Hard
 URL: https://leetcode.com/problems/merge-k-sorted-lists/
 
-Time Complexity: O(n * k) where n is the total number of nodes and k is the number of lists
-Space Complexity: O(1)
+Time Complexity: O(N log k) where N is total number of nodes and k is number of lists
+Space Complexity: O(k) for the heap
 """
 
 # Definition for singly-linked list.
@@ -13,27 +13,29 @@ Space Complexity: O(1)
 #         self.val = val
 #         self.next = next
 
+
 """
 Intuition:
-For each iteration: 
-go through each node in list, keep track of node with smallest val
-add node to new list, move node to next
+Maintain a sorted data structure 
 """
 
 from typing import List, Optional
+import heapq
 
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = res = ListNode()
-        while True:
-            minNode, minIdx = ListNode(float("inf")), -1
-            for idx, node in enumerate(lists):
-                if node and node.val < minNode.val:
-                    minNode = node
-                    minIdx = idx
-            if minIdx == -1:
-                break
-            res.next = minNode
-            res, lists[minIdx] = res.next, lists[minIdx].next
+        dummy = node = ListNode()
+        heap = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i))
+
+        while heap:
+            val, i = heapq.heappop(heap)
+            node.next = ListNode(val)
+            node, lists[i] = node.next, lists[i].next
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i))
+
         return dummy.next
