2df7e560d06c522548e8fdb3e59de090f9cfe446
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2025-12-14 13:30:39 -0500

    Binary Tree Inorder Traversal

--- problems/medium/tree-preorder-traversal/solution.py
+++ problems/medium/tree-preorder-traversal/solution.py
@@ -1,7 +1,7 @@
 """
-Problem: Binary Tree Inorder Traversal
+Problem: Binary Tree Preorder Traversal
 Difficulty: Medium
-URL: https://leetcode.com/problems/binary-tree-inorder-traversal/
+URL: https://leetcode.com/problems/binary-tree-preorder-traversal/
 
 Time Complexity: O(n) - n is the number of nodes in the tree
 Space Complexity: O(h) - h is the height of the tree
@@ -17,13 +17,13 @@ Space Complexity: O(h) - h is the height of the tree
 from typing import List, Optional
 
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         lst = []
 
         def traverse(node: Optional[TreeNode]):
             if node:
-                traverse(node.left)
                 lst.append(node.val)
+                traverse(node.left)
                 traverse(node.right)
 
         traverse(root)
