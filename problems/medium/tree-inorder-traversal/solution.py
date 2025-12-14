907851905e58e7c0c95130cdf926464cba58e4b1
shubhankar-420 <shubhankarvalimbe@gmail.com>
2025-12-14 10:35:07 -0500

    Binary Tree Preorder Traversal

---
+++ problems/medium/tree-inorder-traversal/solution.py
@@ -0,0 +1,29 @@
+"""
+Problem: Binary Tree Preorder Traversal
+Difficulty: Medium
+URL: https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+Time Complexity: O(n) - n is the number of nodes in the tree
+Space Complexity: O(h) - h is the height of the tree
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        lst = []
+
+        def traverse(node: Optional[TreeNode]):
+            if node:
+                lst.append(node.val)
+                traverse(node.left)
+                traverse(node.right)
+
+        traverse(root)
+        return lst
