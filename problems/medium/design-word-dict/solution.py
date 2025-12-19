36f154b3622579880dbec36fee6ca99cadb6ee9d
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2025-12-19 14:44:27 -0500

    Design Add and Search Word Data Structure

---
+++ problems/medium/design-word-dict/solution.py
@@ -0,0 +1,49 @@
+"""
+Problem: Design Add and Search Word Data Structure
+Difficulty: Medium
+URL: https://leetcode.com/problems/design-add-and-search-words-data-structure/
+
+Time Complexity:
+addWord(): O(m) where m is the word length
+search(): O(26^2 * m) = O(m) where m is the word length
+
+Space Complexity: O(N * M) where N is the number of words and M is the average word length
+"""
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}  # val -> node
+        self.isWord = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.isWord = True
+
+    def search(self, word: str) -> bool:
+        ans = False
+
+        def dfs(node, i):
+            nonlocal ans
+            if node:
+                if i == len(word):
+                    if node.isWord:
+                        ans = True
+                elif word[i] == ".":
+                    for n in node.children.values():
+                        dfs(n, i + 1)
+                elif word[i] in node.children:
+                    dfs(node.children[word[i]], i + 1)
+
+        dfs(self.root, 0)
+        return ans
