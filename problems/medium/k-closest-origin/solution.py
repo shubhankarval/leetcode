afa02ae8266feb12dcbd7a0fcfa499241e721a4f
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2025-12-20 15:43:26 -0500

    K Closest Points to Origin - optimal

--- problems/medium/k-closest-origin/solution.py
+++ problems/medium/k-closest-origin/solution.py
@@ -3,15 +3,16 @@ Problem: K Closest Points to Origin
 Difficulty: Medium
 URL: https://leetcode.com/problems/k-closest-points-to-origin/
 
-Time Complexity: O(n + k log n) where n is number of points and k is number of closest points to return
-Space Complexity: O(n) where n is number of points
+Time Complexity: O(n log k)
+Space Complexity: O(k)
+
+where n is the number of points and k is the number of closest points to return.
 """
 
 """
 Intuition:
 Get dist for all points from origin to create a list like [[d, [x, y]]...]
-heapify this list
-return first k elements
+Use maxheap to store this, and pop if len(maxHeap) > k
 """
 
 import math
@@ -20,15 +21,11 @@ import heapq
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        heap = []
+        maxHeap = []
         for x, y in points:
             dist = math.sqrt(x**2 + y**2)
-            heap.append([dist, [x, y]])
+            heapq.heappush(maxHeap, [-dist, [x, y]])
+            if len(maxHeap) > k:
+                heapq.heappop(maxHeap)
 
-        heapq.heapify(heap)
-        res = []
-
-        while len(res) != k:
-            res.append(heapq.heappop(heap)[1])
-
-        return res
+        return [pos for _, pos in maxHeap]
