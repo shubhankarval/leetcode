086a55cf0ca730ab45b74ef5703b07cb1974fd00
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2025-12-22 05:09:03 -0500

    Task Scheduler - optimal using hash map

--- problems/medium/task-scheduler/solution.py
+++ problems/medium/task-scheduler/solution.py
@@ -3,9 +3,9 @@ Problem: Task Scheduler
 Difficulty: Medium
 URL: https://leetcode.com/problems/task-scheduler/
 
-Time Complexity: O(n + k log k) where n is number of tasks, k is number of unique tasks
+Time Complexity: O(n log k) where n is number of tasks, k is number of unique tasks
 Space Complexity: O(k) where k is number of unique tasks
-here, k <= 26
+here, k ≤ 26
 """
 
 """
@@ -15,33 +15,32 @@ track next avlbl slot for task, update it when task finishes
 if count of task reaches 0, remove task
 """
 
-from collections import Counter
+from collections import Counter, defaultdict
 import heapq
 
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        pq = [[-cnt, 0] for cnt in Counter(tasks).values()]
-        heapq.heapify(pq)
+        heap = [-cnt for cnt in Counter(tasks).values()]
+        heapq.heapify(heap)
+        cooldown = defaultdict(list)  # cycle -> list of tasks
         cycle = 0
 
-        while len(pq):
-            removed = []
-            while len(pq) and pq[0][1] > cycle:
-                removed.append(heapq.heappop(pq))
+        while len(heap) or len(cooldown):
+            if cycle in cooldown:
+                for task in cooldown[cycle]:
+                    heapq.heappush(heap, task)
+                del cooldown[cycle]
 
-            if not len(pq):
-                pq = removed
+            if not len(heap):
                 cycle += 1
                 continue
 
-            task = heapq.heappop(pq)
-            task[0] += 1
-            if task[0]:
-                task[1] = cycle + n + 1
-                heapq.heappush(pq, task)
+            task = heapq.heappop(heap)
+            task += 1
+            if task:
+                cooldown[cycle + n + 1].append(task)
 
-            pq = removed + pq
             cycle += 1
 
         return cycle
