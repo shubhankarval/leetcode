4b4471ba37ab3ddce14104ce66d845c8cad76968
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2025-12-22 14:50:41 -0500

    Find Median From Data Stream - optimal

--- problems/hard/find-median/solution.py
+++ problems/hard/find-median/solution.py
@@ -3,23 +3,24 @@ Problem: Find Median From Data Stream
 Difficulty: Hard
 URL: https://leetcode.com/problems/find-median-from-data-stream/
 
-Time Complexity: O(n log n) for findMedian(), O(1) for addNum()
+Time Complexity: O(log n) for findMedian(), O(1) for addNum()
 Space Complexity: O(n)
 
 where n is the number of elements in the data stream.
 """
 
+from sortedcontainers import SortedList
+
 
 class MedianFinder:
 
     def __init__(self):
-        self.nums = []
+        self.nums = SortedList([])
 
     def addNum(self, num: int) -> None:
-        self.nums.append(num)
+        self.nums.add(num)
 
     def findMedian(self) -> float:
-        self.nums.sort()
         mid = len(self.nums) // 2
         if len(self.nums) % 2 == 0:
             return (self.nums[mid] + self.nums[mid - 1]) / 2
