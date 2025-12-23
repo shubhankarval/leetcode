de5f66c2ef13a552ba05d68f0f5c3d45e6712593
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2025-12-22 19:51:58 -0500

    Find Median From Data Stream - optimal with heap

--- problems/hard/find-median/solution.py
+++ problems/hard/find-median/solution.py
@@ -9,19 +9,31 @@ Space Complexity: O(n)
 where n is the number of elements in the data stream.
 """
 
-from sortedcontainers import SortedList
+import heapq
 
 
 class MedianFinder:
 
     def __init__(self):
-        self.nums = SortedList([])
+        self.lesser = []  # -nums from first half of list (maxHeap)
+        self.greater = []  # nums from second half of list (minHeap)
 
     def addNum(self, num: int) -> None:
-        self.nums.add(num)
+        if len(self.greater) and num > self.greater[0]:
+            heapq.heappush(self.greater, num)
+        else:
+            heapq.heappush(self.lesser, -num)
+
+        if len(self.lesser) - len(self.greater) > 1:
+            num = -heapq.heappop(self.lesser)
+            heapq.heappush(self.greater, num)
+        elif len(self.greater) - len(self.lesser) > 1:
+            num = heapq.heappop(self.greater)
+            heapq.heappush(self.lesser, -num)
 
     def findMedian(self) -> float:
-        mid = len(self.nums) // 2
-        if len(self.nums) % 2 == 0:
-            return (self.nums[mid] + self.nums[mid - 1]) / 2
-        return self.nums[mid]
+        if len(self.lesser) > len(self.greater):
+            return -self.lesser[0]
+        elif len(self.greater) > len(self.lesser):
+            return self.greater[0]
+        return (-self.lesser[0] + self.greater[0]) / 2
