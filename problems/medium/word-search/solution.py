4e85749df22f1764ff36fe90aaaf25673c555673
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2025-12-27 06:40:56 -0500

    Word Search - most optimal

--- problems/medium/word-search/solution.py
+++ problems/medium/word-search/solution.py
@@ -18,25 +18,33 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
 
-        def dfs(r, c, i, s):
-            if not (0 <= r < rows) or not (0 <= c < cols) or (r, c) in s:
+        def dfs(r, c, i):
+            if (
+                not (0 <= r < rows)
+                or not (0 <= c < cols)
+                or board[r][c] == "#"
+                or board[r][c] != word[i]
+            ):
                 return False
-            if board[r][c] == word[i]:
-                i += 1
-                if i == len(word):
-                    return True
 
-                s.add((r, c))
-                for row, col in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
-                    if dfs(row, col, i, s):
-                        return True
-                s.remove((r, c))
+            i += 1
+            if i == len(word):
+                return True
 
-            return False
+            board[r][c] = "#"
+            res = (
+                dfs(r + 1, c, i)
+                or dfs(r - 1, c, i)
+                or dfs(r, c + 1, i)
+                or dfs(r, c - 1, i)
+            )
+            board[r][c] = word[i - 1]
+
+            return res
 
         for r in range(rows):
             for c in range(cols):
-                if dfs(r, c, 0, set()):
+                if dfs(r, c, 0):
                     return True
 
         return False
