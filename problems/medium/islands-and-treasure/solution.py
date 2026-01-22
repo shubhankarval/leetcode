2e026bda5f8667ac7bce87ad94ce8fb60a823f22
Shubhankar <shubhankarvalimbe@gmail.com>
2026-01-22 18:03:21 -0500

    Islands and Treasure - optimal

--- problems/medium/islands-and-treasure/solution.py
+++ problems/medium/islands-and-treasure/solution.py
@@ -3,7 +3,7 @@ Problem: Islands and Treasure
 Difficulty: Medium
 URL: https://neetcode.io/problems/islands-and-treasure/solution
 
-Time Complexity: O((m * n)²) where m is the number of rows and n is the number of columns in the grid
+Time Complexity: O(m * n) where m is the number of rows and n is the number of columns in the grid
 Space Complexity: O(m * n) for the BFS queue and visited set in the worst case
 """
 
@@ -14,29 +14,31 @@ from collections import deque
 class Solution:
     def islandsAndTreasure(self, grid: List[List[int]]) -> None:
         rows, cols = len(grid), len(grid[0])
-
-        def bfs(r, c):
-            queue = deque([(r, c, 0)])
-            visited = set([(r, c)])
-
-            while queue:
-                curr_r, curr_c, dist = queue.popleft()
-
-                if grid[curr_r][curr_c] > 0:
-                    grid[curr_r][curr_c] = min(grid[curr_r][curr_c], dist)
-
-                for nr, nc in [
-                    (curr_r + 1, curr_c),
-                    (curr_r - 1, curr_c),
-                    (curr_r, curr_c + 1),
-                    (curr_r, curr_c - 1),
-                ]:
-                    if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in visited:
-                        visited.add((nr, nc))
-                        if grid[nr][nc] != -1:
-                            queue.append((nr, nc, dist + 1))
+        queue = deque([])
+        visited = set()
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 0:
-                    bfs(r, c)
+                    queue.append((r, c, 0))
+                    visited.add((r, c))
+
+        while queue:
+            currR, currC, currDist = queue.popleft()
+            if grid[currR][currC] > 0:
+                grid[currR][currC] = min(grid[currR][currC], currDist)
+
+            for newR, newC in [
+                (currR + 1, currC),
+                (currR - 1, currC),
+                (currR, currC + 1),
+                (currR, currC - 1),
+            ]:
+                if (
+                    0 <= newR < rows
+                    and 0 <= newC < cols
+                    and (newR, newC) not in visited
+                ):
+                    visited.add((newR, newC))
+                    if grid[newR][newC] != -1:
+                        queue.append((newR, newC, currDist + 1))
