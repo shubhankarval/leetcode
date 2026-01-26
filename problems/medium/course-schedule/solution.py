b0c8ea6f5ea4d10174445b42b71289007aa0a624
Shubhankar <shubhankarvalimbe@gmail.com>
2026-01-26 11:48:15 -0500

    Course Schedule - BFS Topological sort

--- problems/medium/course-schedule/solution.py
+++ problems/medium/course-schedule/solution.py
@@ -4,14 +4,16 @@ Difficulty: Medium
 URL: https://leetcode.com/problems/course-schedule/
 
 Time Complexity: O(V + E) where V is the number of courses and E is the number of prerequisites
-Space Complexity: O(V + E) for adjacency list, path and recursion stack
+Space Complexity: O(V + E) for adjacency list and path/queue
 """
 
 from typing import List
+from collections import deque
 
 
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    # Cycle detection
+    def canFinishDFS(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         courseToPre = {i: [] for i in range(numCourses)}
         for course, prereq in prerequisites:
             courseToPre[course].append(prereq)
@@ -39,3 +41,28 @@ class Solution:
                 return False
 
         return True
+
+    # Topological sort (Kahn's algorithm)
+    def canFinishBFS(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegree = [0] * numCourses  # no. of incoming edges
+        adjList = {i: [] for i in range(numCourses)}  # prereq -> courses
+
+        for course, prereq in prerequisites:
+            adjList[prereq].append(course)
+            indegree[course] += 1
+
+        queue = deque([])
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                queue.append(course)
+
+        coursesFinished = 0
+        while queue:
+            prereq = queue.popleft()
+            coursesFinished += 1
+            for course in adjList[prereq]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    queue.append(course)
+
+        return coursesFinished == numCourses
