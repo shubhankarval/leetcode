99973bc4a38342f0fa73df5d52539cf6c332077c
Shubhankar <shubhankarvalimbe@gmail.com>
2026-01-26 06:02:43 -0500

    Course Schedule - DFS most optimal

--- problems/medium/course-schedule/solution.py
+++ problems/medium/course-schedule/solution.py
@@ -4,40 +4,38 @@ Difficulty: Medium
 URL: https://leetcode.com/problems/course-schedule/
 
 Time Complexity: O(V + E) where V is the number of courses and E is the number of prerequisites
-Space Complexity: O(V + E) for adjacency list, path, visited and recursion stack
+Space Complexity: O(V + E) for adjacency list, path and recursion stack
 """
 
 from typing import List
-from collections import defaultdict
 
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        courseToPre = defaultdict(list)
+        courseToPre = {i: [] for i in range(numCourses)}
         for course, prereq in prerequisites:
             courseToPre[course].append(prereq)
 
-        path, visited = [False] * numCourses, [False] * numCourses
+        path = [False] * numCourses
 
         # check if cycle exists in graph
         def dfs(course):
             if path[course]:
                 return True
-            if visited[course]:
+            if not courseToPre[course]:
                 return False
             path[course] = True
-            visited[course] = True
 
-            if course in courseToPre:
-                for prereq in courseToPre[course]:
-                    if dfs(prereq):
-                        return True
+            for prereq in courseToPre[course]:
+                if dfs(prereq):
+                    return True
 
             path[course] = False
+            courseToPre[course] = []
             return False
 
         for course in range(numCourses):
-            if not visited[course] and dfs(course):
+            if dfs(course):
                 return False
 
         return True
