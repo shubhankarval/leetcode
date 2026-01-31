7a01000686e6bbb2b6eb4a39898e82cc86c83b61
Shubhankar <shubhankarvalimbe@gmail.com>
2026-01-31 16:43:17 -0500

    Word Ladder - BFS

--- problems/hard/word-ladder/solution.py
+++ problems/hard/word-ladder/solution.py
@@ -3,60 +3,52 @@ Problem: Word Ladder
 Difficulty: Hard
 URL: https://leetcode.com/problems/word-ladder/
 
-Time Complexity: O(n²L + n!) ≈ O(n!) where n is number of words in wordList and L is length of each word
-Space Complexity: O(n²) for wordJump & path dictionaries and recursion stack
-"""
-
-"""
-Intuition:
-compare each word with all other words, see where it can jump to
-run dfs starting from beginWord
-if not possible to reach endWord return maxCnt
-else return min count 
+Time Complexity: O(N² * L) where N is the number of words in wordList and L is the length of each word.
+Space Complexity: O(N²) for the adjacency map and O(N) for the queue and visited set.
 """
 
 from typing import List
+from collections import deque
 
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        path = {word: 0 for word in wordList}
-        if endWord not in path:
+        adjMap = {}
+        beginIdx = endIdx = -1
+        for i, word in enumerate(wordList):
+            adjMap[i] = self.addAdjacentNodes(word, i, wordList)
+            if word == beginWord:
+                beginIdx = i
+            elif word == endWord:
+                endIdx = i
+        if endIdx == -1:
             return 0
-        path[beginWord] = 0
-        maxCnt = len(wordList) + 2  # beyond possible count of words
+        if beginIdx == -1:
+            adjMap[beginIdx] = self.addAdjacentNodes(beginWord, beginIdx, wordList)
 
-        wordJump = {}
-        for i, word in enumerate(wordList):
-            wordJump[word] = self.compareWords(word, i, wordList)
-        if beginWord not in wordJump:
-            wordJump[beginWord] = self.compareWords(beginWord, -1, wordList)
+        queue = deque([[beginIdx, 1]])  # idx, cnt
+        visited = set([beginIdx])  # set of indices
 
-        def dfs(currWord, cnt):
-            if currWord == endWord:
+        while queue:
+            currIdx, cnt = queue.popleft()
+            if currIdx == endIdx:
                 return cnt
-            path[currWord] = 1
-            minCnt = maxCnt
+            for i in adjMap[currIdx]:
+                if i not in visited:
+                    queue.append([i, cnt + 1])
+                    visited.add(i)
 
-            for word in wordJump[currWord]:
-                if not path[word]:
-                    minCnt = min(dfs(word, cnt + 1), minCnt)
+        return 0
 
-            path[currWord] = 0
-            return minCnt
-
-        cnt = dfs(beginWord, 1)
-        return cnt if cnt < maxCnt else 0
-
-    def compareWords(self, currWord: str, currIdx: int, wordList: str):
-        jumpWords = []
-        for i, word in enumerate(wordList):
-            if i != currIdx:
-                j = diff = 0
-                while diff <= 1 and j < len(word):
-                    if word[j] != currWord[j]:
+    def addAdjacentNodes(self, currWord, currIdx, wordList):
+        wordIndices = []
+        for wordIdx, word in enumerate(wordList):
+            if wordIdx != currIdx:
+                diff = i = 0
+                while diff <= 1 and i < len(currWord):
+                    if word[i] != currWord[i]:
                         diff += 1
-                    j += 1
+                    i += 1
                 if diff == 1:
-                    jumpWords.append(word)
-        return jumpWords
+                    wordIndices.append(wordIdx)
+        return wordIndices
