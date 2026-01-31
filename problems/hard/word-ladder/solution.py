50bd6152b58fef925b20d431d25d047f5251b1f7
Shubhankar <shubhankarvalimbe@gmail.com>
2026-01-31 05:14:17 -0500

    Word Ladder - slightly faster

--- problems/hard/word-ladder/solution.py
+++ problems/hard/word-ladder/solution.py
@@ -3,8 +3,8 @@ Problem: Word Ladder
 Difficulty: Hard
 URL: https://leetcode.com/problems/word-ladder/
 
-Time Complexity: O(n! * L) wghere n is number of words in wordList and L is length of each word
-Space Complexity: O(n) for path dictionary and recursion stack
+Time Complexity: O(n²L * n!) ≈ O(n!) where n is number of words in wordList and L is length of each word
+Space Complexity: O(n²) for wordJump & path dictionaries and recursion stack
 """
 
 """
@@ -26,25 +26,37 @@ class Solution:
         path[beginWord] = 0
         maxCnt = len(wordList) + 2  # beyond possible count of words
 
+        wordJump = {}
+        for i, word in enumerate(wordList):
+            wordJump[word] = self.compareWords(word, i, wordList)
+        if beginWord not in wordJump:
+            wordJump[beginWord] = self.compareWords(beginWord, -1, wordList)
+
         def dfs(currWord, cnt):
             if currWord == endWord:
                 return cnt
-
             path[currWord] = 1
             minCnt = maxCnt
-            for word in wordList:
+
+            for word in wordJump[currWord]:
                 if not path[word]:
-                    # compare currword with word, if only 1 diff then dfs to it
-                    i = diff = 0
-                    while diff <= 1 and i < len(word):
-                        if word[i] != currWord[i]:
-                            diff += 1
-                        i += 1
-                    if diff == 1:
-                        minCnt = min(dfs(word, cnt + 1), minCnt)
+                    minCnt = min(dfs(word, cnt + 1), minCnt)
 
             path[currWord] = 0
             return minCnt
 
         cnt = dfs(beginWord, 1)
         return cnt if cnt < maxCnt else 0
+
+    def compareWords(self, currWord: str, currIdx: int, wordList: str):
+        jumpWords = []
+        for i, word in enumerate(wordList):
+            if i != currIdx:
+                j = diff = 0
+                while diff <= 1 and j < len(word):
+                    if word[j] != currWord[j]:
+                        diff += 1
+                    j += 1
+                if diff == 1:
+                    jumpWords.append(word)
+        return jumpWords
