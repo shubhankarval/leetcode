4b9423b75173e56e0c79616007a4d060a87bb82a
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-06 18:57:21 -0500

    Climbing Stairs

--- problems/easy/climbing-stairs/solution.py
+++ problems/easy/climbing-stairs/solution.py
@@ -10,17 +10,15 @@ Space Complexity: O(n)
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [0] * (n + 2)
+        dp = [0] * n
 
         def rec(steps):
             if steps > n:
                 return 0
             if steps == n:
                 return 1
-            if not dp[steps + 1]:
-                dp[steps + 1] = rec(steps + 1)
-            if not dp[steps + 2]:
-                dp[steps + 2] = rec(steps + 2)
-            return dp[steps + 1] + dp[steps + 2]
+            if not dp[steps]:
+                dp[steps] = rec(steps + 1) + rec(steps + 2)
+            return dp[steps]
 
         return rec(0)
