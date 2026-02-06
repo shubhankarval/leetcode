cec48da9024ba313fdca0881a95f7700687e0a49
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-06 14:24:40 -0500

    Fibonacci Number

--- problems/easy/fibonacci-number/solution.py
+++ problems/easy/fibonacci-number/solution.py
@@ -10,10 +10,8 @@ Space Complexity: O(1)
 
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
+        if n == 0 or n == 1:
+            return n
         n1, n2 = 0, 1
         fibSum = 0
         while n > 1:
