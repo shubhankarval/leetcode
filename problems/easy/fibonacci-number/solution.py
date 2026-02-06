406b96726153678008ce9970f628650a2dc668cd
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-06 14:48:44 -0500

    Fibonacci Number - multiple soln

--- problems/easy/fibonacci-number/solution.py
+++ problems/easy/fibonacci-number/solution.py
@@ -9,6 +9,7 @@ Space Complexity: O(1)
 
 
 class Solution:
+    # Iterative
     def fib(self, n: int) -> int:
         if n == 0 or n == 1:
             return n
@@ -18,3 +19,23 @@ class Solution:
             n1, n2 = n2, n1 + n2
             n -= 1
         return n1 + n2
+
+    # Recursive (suboptimal)
+    def fib(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return n
+        return self.fib(n - 1) + self.fib(n - 2)
+
+    # Recursive DP
+    def fib(self, n: int) -> int:
+        dp = [0] * (n + 1)
+
+        def rec(n):
+            if n == 0 or n == 1:
+                return n
+            if dp[n]:
+                return dp[n]
+            dp[n] = rec(n - 1) + rec(n - 2)
+            return dp[n]
+
+        return rec(n)
