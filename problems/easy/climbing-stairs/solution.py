3cc9d4ecb1aceb94fb9d3af0a7d0b3e78a94f2cc
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-08 11:39:17 -0500

    Min Cost Climbing Stairs

--- problems/easy/climbing-stairs/solution.py
+++ problems/easy/climbing-stairs/solution.py
@@ -9,8 +9,24 @@ Space Complexity: O(1)
 
 
 class Solution:
+    # DP Bottom-up
     def climbStairs(self, n: int) -> int:
         n1, n2 = 1, 1
         for _ in range(2, n + 1):
             n1, n2 = n2, n1 + n2
         return n2
+
+    # DP Top-down
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * n
+
+        def rec(steps):
+            if steps > n:
+                return 0
+            if steps == n:
+                return 1
+            if not dp[steps]:
+                dp[steps] = rec(steps + 1) + rec(steps + 2)
+            return dp[steps]
+
+        return rec(0)
--- problems/easy/min-cost-stairs/solution.py
+++ problems/easy/min-cost-stairs/solution.py
@@ -4,13 +4,21 @@ Difficulty: Easy
 URL: https://leetcode.com/problems/min-cost-climbing-stairs/
 
 Time Complexity: O(n)
-Space Complexity: O(n)
+Space Complexity: O(1)
 """
 
 from typing import List
 
 
 class Solution:
+    # DP Bottom-up
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        one, two = 0, 0
+        for i in range(2, len(cost) + 1):
+            one, two = min(one + cost[i - 1], two + cost[i - 2]), one
+        return one
+
+    # DP Top-down
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         cost.append(0)
         n = len(cost)
