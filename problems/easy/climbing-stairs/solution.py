f2ad8c39a3ea4152548ec130ca860665253d78bd
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-08 11:13:18 -0500

    Climbing Stairs - most optimal

--- problems/easy/climbing-stairs/solution.py
+++ problems/easy/climbing-stairs/solution.py
@@ -4,21 +4,13 @@ Difficulty: Easy
 URL: https://leetcode.com/problems/climbing-stairs/
 
 Time Complexity: O(n)
-Space Complexity: O(n)
+Space Complexity: O(1)
 """
 
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [0] * n
-
-        def rec(steps):
-            if steps > n:
-                return 0
-            if steps == n:
-                return 1
-            if not dp[steps]:
-                dp[steps] = rec(steps + 1) + rec(steps + 2)
-            return dp[steps]
-
-        return rec(0)
+        n1, n2 = 1, 1
+        for _ in range(2, n + 1):
+            n1, n2 = n2, n1 + n2
+        return n2
