04f26390f3d55089fc2b03323e0a2e522cf470cd
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-08 07:14:36 -0500

    Decode Ways

--- problems/medium/decode-ways/solution.py
+++ problems/medium/decode-ways/solution.py
@@ -3,19 +3,23 @@ Problem: Decode Ways
 Difficulty: Medium
 URL: https://leetcode.com/problems/decode-ways/
 
-Time Complexity: O(2ⁿ)
+Time Complexity: O(n)
 Space Complexity: O(n)
 """
 
 
 class Solution:
     def numDecodings(self, s: str) -> int:
+        dp = {}
+
         def rec(idx, prev):
             if idx >= len(s):
                 return 1
-            curr = prev + s[idx]
-            if not 1 <= int(curr) <= 26:
-                return 0
-            return rec(idx + 1, "") + rec(idx + 1, curr)
+            if (idx, prev) not in dp:
+                curr = prev + s[idx]
+                if not 1 <= int(curr) <= 26:
+                    return 0
+                dp[(idx, prev)] = rec(idx + 1, "") + rec(idx + 1, curr)
+            return dp[(idx, prev)]
 
         return rec(0, "") // 2
