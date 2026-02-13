3495be7d259d70ca4deae6f984ffd730ab3cd145
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-13 17:28:06 -0500

    Maximum Product Subarray

--- problems/medium/maximum-product-subarray/solution.py
+++ problems/medium/maximum-product-subarray/solution.py
@@ -3,7 +3,7 @@ Problem: Maximum Product Subarray
 Difficulty: Medium
 URL: https://leetcode.com/problems/maximum-product-subarray/
 
-Time Complexity: O(n²)
+Time Complexity: O(n)
 Space Complexity: O(1)
 """
 
@@ -12,13 +12,39 @@ from typing import List
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        res = float("-inf")
-        for i in range(len(nums)):
-            prod = maxProd = nums[i]
-            for j in range(i + 1, len(nums)):
-                prod *= nums[j]
-                maxProd = max(prod, maxProd)
-                if prod == 0:
+        def trim_negative_edge(start, end):
+            if end - start == 1:
+                return 1
+            left = right = 1
+            for i in range(start, end):
+                left *= nums[i]
+                if nums[i] < 0:
                     break
-            res = max(res, maxProd)
-        return res
+            for i in range(end - 1, start - 1, -1):
+                right *= nums[i]
+                if nums[i] < 0:
+                    break
+            return max(left, right)
+
+        start = end = 0
+        prod, max_prod = 1, float("-inf")
+
+        for i, num in enumerate(nums):
+            if num == 0:
+                if start < i:
+                    if prod < 0:
+                        prod //= trim_negative_edge(start, end)
+                    max_prod = max(prod, max_prod)
+                max_prod = max(0, max_prod)
+                start = end = i + 1
+                prod = 1
+            else:
+                prod *= num
+                end += 1
+
+        if start < len(nums):
+            if prod < 0:
+                prod //= trim_negative_edge(start, end)
+            max_prod = max(max_prod, prod)
+
+        return max_prod
