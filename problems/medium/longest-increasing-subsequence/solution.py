51f1976aa2f07afa5c753a3ac266664b75d64d09
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-19 06:05:19 -0500

    Longest Increasing Subsequence - Bottom-Up

--- problems/medium/longest-increasing-subsequence/solution.py
+++ problems/medium/longest-increasing-subsequence/solution.py
@@ -7,14 +7,23 @@ Time Complexity: O(n²) where n is the length of the input array
 Space Complexity: O(n) for the dp array and the recursion stack
 """
 
-"""
-Intuition: DFS with DP
-"""
-
 from typing import List
 
 
 class Solution:
+    # DP Bottom-Up
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [0] * len(nums)
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], dp[j])
+            dp[i] += 1
+
+        return max(dp)
+
+    # DP Top-Down
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [0] * len(nums)
 
