3b8126466714b3daaa7901e44e8911f3ddf59cf1
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-20 16:35:34 -0500

    Longest Palindromic Substring - optimal

--- problems/medium/longest-palindromic-substring/solution.py
+++ problems/medium/longest-palindromic-substring/solution.py
@@ -3,30 +3,29 @@ Problem: Longest Palindromic Substring
 Difficulty: Medium
 URL: https://leetcode.com/problems/longest-palindromic-substring/
 
-Time Complexity: O(n³)
-Space Complexity: O(n)
+Time Complexity: O(n²)
+Space Complexity: O(1)
 """
 
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
-        for i in range(len(s)):
-            j = i + len(res)
-            if j >= len(s):
-                break
-            word = s[i:j]
-            for k in range(j, len(s)):
-                word += s[k]
-                if self.isPalindrome(word):
-                    res = word
-        return res
+        def getPalindrome(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return l + 1, r
 
-    def isPalindrome(self, s):
-        l, r = 0, len(s) - 1
-        while l < r:
-            if s[l] != s[r]:
-                return False
-            l += 1
-            r -= 1
-        return True
+        start = end = 0
+        for i in range(len(s)):
+            # odd length
+            l, r = getPalindrome(i, i)
+            if r - l > end - start:
+                start, end = l, r
+
+            # even length
+            l, r = getPalindrome(i, i + 1)
+            if r - l > end - start:
+                start, end = l, r
+
+        return s[start:end]
