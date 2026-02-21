4df5a95f20b5808d6987ec5be4f79b624f81f88b
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-20 19:53:58 -0500

    Word Break - optimal

--- problems/medium/word-break/solution.py
+++ problems/medium/word-break/solution.py
@@ -3,14 +3,35 @@ Problem: Word Break
 Difficulty: Medium
 URL: https://leetcode.com/problems/word-break/
 
-Time Complexity: O(n³) where n is the length of string s
-Space Complexity: O(n + m) where n is the length of string s and m is the number of words in wordDict
+Time Complexity: O(n * m * t)
+Space Complexity: O(n)
+- where n = length of string s,
+        m = number of words in wordDict,
+        t = maximum length of a word in wordDict
 """
 
 from typing import List
 
 
 class Solution:
+    # DP Top-Down (Optimimal)
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = [None] * (len(s) + 1)
+        memo[len(s)] = True
+
+        def dfs(i):
+            if memo[i] == None:
+                memo[i] = False
+                for word in wordDict:
+                    wordEnd = i + len(word)
+                    if wordEnd <= len(s) and s[i:wordEnd] == word and dfs(wordEnd):
+                        memo[i] = True
+                        break
+            return memo[i]
+
+        return dfs(0)
+
+    # DP Top-Down (Suboptimal)
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wordSet = set(wordDict)
         memo = [None] * len(s)
