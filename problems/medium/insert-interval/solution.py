62520fbfebe50d1a9ebd5c3d40d335788a104b48
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-22 09:46:47 -0500

    Insert Interval - Greedy

--- problems/medium/insert-interval/solution.py
+++ problems/medium/insert-interval/solution.py
@@ -4,7 +4,7 @@ Difficulty: Medium
 URL: https://leetcode.com/problems/insert-interval/
 
 Time Complexity: O(n)
-Space Complexity: O(n)
+Space Complexity: O(1)
 """
 
 from typing import List
@@ -14,38 +14,18 @@ class Solution:
     def insert(
         self, intervals: List[List[int]], newInterval: List[int]
     ) -> List[List[int]]:
-        start, end = newInterval
-        startIdx = endIdx = -1
-        i = 0
+        res = []
 
-        while endIdx == -1 and i < len(intervals):
-            currStart, currEnd = intervals[i]
-            # start
-            if startIdx == -1:
-                if currStart <= start <= currEnd:
-                    startIdx = i
-                elif currStart > start:
-                    startIdx = i
-                    intervals.insert(i, [start, start])
-                    continue
-
-            # end
-            if currStart <= end <= currEnd:
-                endIdx = i
-            elif currStart > end:
-                endIdx = i
-                intervals.insert(i, [end, end])
-
-            i += 1
-
-        if startIdx == -1:
-            return intervals + [newInterval]
-        if endIdx == -1:
-            endIdx = len(intervals)
-            intervals.append([end, end])
-
-        return (
-            intervals[:startIdx]
-            + [[intervals[startIdx][0], intervals[endIdx][1]]]
-            + (intervals[endIdx + 1 :] if endIdx != len(intervals) - 1 else [])
-        )
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1]),
+                ]
+        res.append(newInterval)
+        return res
