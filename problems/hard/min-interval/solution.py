11b6ce3e72673f6debd7b35ce493a178cdec85a5
Shubhankar <shubhankarvalimbe@gmail.com>
2026-02-28 15:49:59 -0500

    Minimum Interval to Include Each Query

--- problems/hard/min-interval/solution.py
+++ problems/hard/min-interval/solution.py
@@ -3,11 +3,10 @@ Problem: Minimum Interval to Include Each Query
 Difficulty: Hard
 URL: https://leetcode.com/problems/minimum-interval-to-include-each-query/
 
-Time Complexity: O(nlogn + n*k + m)
-Space Complexity: O(k)
+Time Complexity: O(nlogn + n*m)
+Space Complexity: O(n)
 - where n = length of intervals
         m = length of queries
-        k = max - min time in intervals
 """
 
 from typing import List
@@ -15,13 +14,15 @@ from typing import List
 
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        intervals.sort(key=lambda x: x[1] - x[0])
-        out = {}  # time -> len
-
-        for start, end in intervals:
-            diff = end - start + 1
-            for i in range(start, end + 1):
-                if i not in out:
-                    out[i] = diff
-
-        return [out[time] if time in out else -1 for time in queries]
+        intLength = sorted(
+            list(map(lambda x: [x[0], x[1], x[1] - x[0] + 1], intervals)),
+            key=lambda x: x[2],
+        )
+        res = []
+        for q in queries:
+            res.append(-1)
+            for start, end, length in intLength:
+                if start <= q <= end:
+                    res[-1] = length
+                    break
+        return res
