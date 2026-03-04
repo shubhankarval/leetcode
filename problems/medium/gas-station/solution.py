b5a00ec4df11b2bd649973d4d901f5667b6b79e2
Shubhankar <shubhankarvalimbe@gmail.com>
2026-03-04 17:14:46 -0500

    Gas Station

--- problems/medium/gas-station/solution.py
+++ problems/medium/gas-station/solution.py
@@ -3,7 +3,7 @@ Problem: Gas Station
 Difficulty: Medium
 URL: https://leetcode.com/problems/gas-station/
 
-Time Complexity: O(n²)
+Time Complexity: O(n)
 Space Complexity: O(1)
 """
 
@@ -12,14 +12,19 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # If total gas is less than total cost, it's mathematically impossible
+        if sum(gas) < sum(cost):
+            return -1
+
+        total_tank = 0
+        start_idx = 0
+
         for i in range(len(gas)):
-            j, currGas = i, gas[i]
+            total_tank += gas[i] - cost[i]
 
-            while currGas - cost[j] >= 0:
-                currGas -= cost[j]
-                j = j + 1 if j < len(gas) - 1 else 0
-                if j == i:
-                    return i
-                currGas += gas[j]
+            # If we run out of gas, this start_idx (and all before it) is invalid
+            if total_tank < 0:
+                start_idx = i + 1
+                total_tank = 0
 
-        return -1
+        return start_idx
