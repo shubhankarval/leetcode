b4cf6c0de95d577e7857b83da31b0ccf61814caa
Shubhankar <shubhankarvalimbe@gmail.com>
2026-03-07 04:50:42 -0500

    Merge Triplets to Form Target - most optimal

--- problems/medium/merge-triplets/solution.py
+++ problems/medium/merge-triplets/solution.py
@@ -12,6 +12,22 @@ from typing import List
 
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        x = y = z = False
+
+        for t in triplets:
+            if t[0] == target[0] and t[1] <= target[1] and t[2] <= target[2]:
+                x = True
+            if t[0] <= target[0] and t[1] == target[1] and t[2] <= target[2]:
+                y = True
+            if t[0] <= target[0] and t[1] <= target[1] and t[2] == target[2]:
+                z = True
+            if x and y and z:
+                return True
+
+        return False
+
+    # Alternative solution
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
         res = [-1, -1, -1]
 
         for triplet in triplets:
