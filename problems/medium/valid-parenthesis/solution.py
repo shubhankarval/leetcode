d57a03ff764e3d00c7108a6b60fd21ba359e425f
Shubhankar <shubhankarvalimbe@gmail.com>
2026-03-07 19:30:06 -0500

    Valid Parenthesis String - DP Top-down

--- problems/medium/valid-parenthesis/solution.py
+++ problems/medium/valid-parenthesis/solution.py
@@ -3,7 +3,7 @@ Problem: Valid Parenthesis String
 Difficulty: Medium
 URL: https://leetcode.com/problems/valid-parenthesis-string/
 
-Time Complexity: O(n³)
+Time Complexity: O(n²)
 Space Complexity: O(n²)
 """
 
@@ -13,25 +13,20 @@ class Solution:
         dp = {}
 
         def dfs(i, c):
+            if c < 0:
+                return False
             if i == len(s):
                 return c == 0
             if (i, c) in dp:
                 return dp[(i, c)]
-            x = c
-            star = False
-            for j in range(i, len(s)):
-                if x < 0:
-                    break
-                if s[j] == "(":
-                    x += 1
-                elif s[j] == ")":
-                    x -= 1
-                else:
-                    dp[(i, c)] = dfs(j + 1, x + 1) or dfs(j + 1, x - 1) or dfs(j + 1, x)
-                    star = True
-                    break
-            if not star:
-                dp[(i, c)] = x == 0
+
+            if s[i] == "(":
+                dp[(i, c)] = dfs(i + 1, c + 1)
+            elif s[i] == ")":
+                dp[(i, c)] = dfs(i + 1, c - 1)
+            else:
+                dp[(i, c)] = dfs(i + 1, c + 1) or dfs(i + 1, c - 1) or dfs(i + 1, c)
+
             return dp[(i, c)]
 
         return dfs(0, 0)
