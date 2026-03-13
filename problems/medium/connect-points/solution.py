cba0b3c1dc5ab2feb12661b2cd18efc944725a90
Shubhankar <shubhankarvalimbe@gmail.com>
2026-03-13 15:33:56 -0400

    Min Cost to Connect Points - standard Prim's

--- problems/medium/connect-points/solution.py
+++ problems/medium/connect-points/solution.py
@@ -8,32 +8,33 @@ Space Complexity: O(n²)
 """
 
 from typing import List
+from collections import defaultdict
+import heapq
 
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        visited = {}  # idx -> sorted list of [dist, idx]
-        i = 0
+        adjList = defaultdict(list)
+        for i, (x1, y1) in enumerate(points):
+            for j in range(i + 1, len(points)):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adjList[i].append([dist, j])
+                adjList[j].append([dist, i])
+
         cost = 0
+        minHeap = [[0, 0]]
+        visited = set()
 
-        for _ in range(len(points) - 1):
-            x1, y1 = points[i]
-            visited[i] = []
+        while len(visited) < len(points):
+            dist, i = heapq.heappop(minHeap)
+            if i in visited:
+                continue
+            cost += dist
+            visited.add(i)
 
-            for j, (x2, y2) in enumerate(points):
-                if j not in visited:
-                    dist = abs(x1 - x2) + abs(y1 - y2)
-                    visited[i].append([dist, j])
-
-            visited[i].sort(reverse=True)
-            minDist = float("inf")
-
-            for j in visited:
-                while visited[j][-1][1] in visited:
-                    visited[j].pop()
-                if visited[j][-1][0] < minDist:
-                    minDist, i = visited[j][-1]
-
-            cost += minDist
+            for neiDist, nei in adjList[i]:
+                if nei not in visited:
+                    heapq.heappush(minHeap, [neiDist, nei])
 
         return cost
