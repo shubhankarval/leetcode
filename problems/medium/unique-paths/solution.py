6ae3f3261629574849028cdd73f9ed9d94089ea8
Shubhankar <shubhankarvalimbe@gmail.com>
2026-03-18 08:39:32 -0400

    Unique Paths - optimal

--- problems/medium/unique-paths/solution.py
+++ problems/medium/unique-paths/solution.py
@@ -3,25 +3,24 @@ Problem: Unique Paths
 Difficulty: Medium
 URL: https://leetcode.com/problems/unique-paths/
 
-Time Complexity: O(2 ^ (m + n))
-Space Complexity: O(m + n)
+Time Complexity: O(m * n)
+Space Complexity: O(m * n)
 """
 
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dirs = [[1, 0], [0, 1]]
+        dp = [[0] * n for _ in range(m)]
+        dp[m - 1][n - 1] = 1
 
         def dfs(r, c):
-            if r == m - 1 and c == n - 1:
-                return 1
-            paths = 0
+            if not dp[r][c]:
+                for dr, dc in dirs:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < m and 0 <= nc < n:
+                        dp[r][c] += dfs(nr, nc)
 
-            for dr, dc in dirs:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < m and 0 <= nc < n:
-                    paths += dfs(nr, nc)
-
-            return paths
+            return dp[r][c]
 
         return dfs(0, 0)
