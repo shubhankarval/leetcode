51fd241f99aab7b0eb2882b4ecd922fe19b59b68
Shubhankar <shubhankarvalimbe@gmail.com>
2026-03-21 09:24:43 -0400

    Best Time to Buy and Sell Stock with Cooldown - optimal

--- problems/medium/buy-sell-stock-cooldown/solution.py
+++ problems/medium/buy-sell-stock-cooldown/solution.py
@@ -3,8 +3,8 @@ Problem: Best Time to Buy and Sell Stock with Cooldown
 Difficulty: Medium
 URL: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
 
-Time Complexity: O(2ⁿ)
-Space Complexity: O(n)
+Time Complexity: O(n)
+Space Complexity: O(1)
 """
 
 from typing import List
@@ -12,18 +12,25 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
+        n = len(prices)
+        buy = [0, 0]
+        sell = 0
 
-        def dfs(i, buy, profit):
-            nonlocal res
-            if i >= len(prices):
-                return
-            for j in range(i, len(prices)):
-                if buy:
-                    dfs(j + 1, False, profit - prices[j])
-                else:
-                    res = max(res, profit + prices[j])
-                    dfs(j + 2, True, profit + prices[j])
+        for i in range(n - 1, -1, -1):
+            newBuy = max(buy[0], sell - prices[i])
+            newSell = max(sell, buy[1] + prices[i])
+            buy[0], buy[1], sell = newBuy, buy[0], newSell
 
-        dfs(0, True, 0)
-        return res
+        return buy[0]
+
+    # Alternative solution with O(n) space complexity
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        buy = [0] * (n + 2)
+        sell = [0] * (n + 2)
+
+        for i in range(n - 1, -1, -1):
+            buy[i] = max(buy[i + 1], sell[i + 1] - prices[i])
+            sell[i] = max(sell[i + 1], buy[i + 2] + prices[i])
+
+        return buy[0]
