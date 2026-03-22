e53906542dfbf3ac88a6c9d79be0bf95c9ee226f
Shubhankar <shubhankarvalimbe@gmail.com>
2026-03-22 09:57:05 -0400

    Coin Change II

--- problems/medium/coin-change-ii/solution.py
+++ problems/medium/coin-change-ii/solution.py
@@ -3,7 +3,7 @@ Problem: Coin Change II
 Difficulty: Medium
 URL: https://leetcode.com/problems/coin-change-ii/
 
-Time Complexity: O(n² * a)
+Time Complexity: O(n * a)
 Space Complexity: O(n * a)
 - where n = no. of coins
         a = amount
@@ -18,15 +18,14 @@ class Solution:
         dp = [[-1] * n for _ in range(amount)]
 
         def dfs(amt, i):
+            if i == n:
+                return 0
             if amt > amount:
                 return 0
             if amt == amount:
                 return 1
             if dp[amt][i] == -1:
-                cnt = 0
-                for j in range(i, n):
-                    cnt += dfs(amt + coins[j], j)
-                dp[amt][i] = cnt
+                dp[amt][i] = dfs(amt + coins[i], i) + dfs(amt, i + 1)
             return dp[amt][i]
 
         return dfs(0, 0)
