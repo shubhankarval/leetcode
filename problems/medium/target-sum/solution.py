8fb9775d158958deef3332c2c140b214ff90ab68
Shubhankar <shubhankarvalimbe@gmail.com>
2026-03-23 15:50:25 -0400

    Target Sum - optimal

--- problems/medium/target-sum/solution.py
+++ problems/medium/target-sum/solution.py
@@ -4,15 +4,29 @@ Difficulty: Medium
 URL: https://leetcode.com/problems/target-sum/
 
 Time Complexity: O(n * s)
-Space Complexity: O(n * s)
+Space Complexity: O(s)
 - where n = number of elements in nums
         s = sum of all elements in nums
 """
 
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = {0: 1}  # amount -> count
+
+        for num in nums:
+            dp2 = defaultdict(int)
+            for amt in dp:
+                dp2[amt + num] += dp[amt]
+                dp2[amt - num] += dp[amt]
+            dp = dp2
+
+        return dp[target] if target in dp else 0
+
+    # DP Top-Down (Space Complexity: O(n * s))
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n = len(nums)
         dp = {}
