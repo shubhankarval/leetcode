8474c8f93ce43c330cec3bc4245f1a0304a7dbca
Shubhankar <shubhankarvalimbe@gmail.com>
2026-04-13 19:34:36 -0400

    Number of 1 Bits - most optimal

--- problems/easy/number-of-1-bits/solution.py
+++ problems/easy/number-of-1-bits/solution.py
@@ -12,6 +12,14 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         cnt = 0
         while n:
+            n &= n - 1
+            cnt += 1
+        return cnt
+
+    # Alternative solution
+    def hammingWeight(self, n: int) -> int:
+        cnt = 0
+        while n:
             if n & 1:
                 cnt += 1
             n >>= 1
